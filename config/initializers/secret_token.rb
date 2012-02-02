9611442a137c12e7df99918e6659cba3f741af00
Andreas Gebhardt <and-geb@gmx.de>
2012-02-02 14:33:25 +0100

    Status_eins

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MlclspApp::Application.config.secret_token = 'e6f18d4677df99176b5a416154c9d9f7e3cb9f79904a86942020a1def9c490ca5836f92b88017a57c7d7ebc02d11f89d8e0afcd261f29939a6e12db41e353c54'
